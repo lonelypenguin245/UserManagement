a3d7d077da3d7be5a84576ae7b165d6908347e3e
lonelypenguin245 <144401532+lonelypenguin245@users.noreply.github.com>
2023-10-06 09:16:45 +0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package UserManagement;
+
+/**
+ *
+ * @author HI
+ */
+public class Main {
+    public static void main(String[] args) {
+        //loop until user want to exit
+        while (true) {
+            int choice = Manager.menu();
+            switch (choice) {
+                case 1:
+                    Manager.createNewAccount();
+                    break;
+                case 2:
+                    Manager.loginSystem();
+                    break;
+                case 3:
+                    return;
+            }
+        }
+    }
+}
